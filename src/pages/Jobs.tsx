5d98f868624b9c09f30dff7e6831162ee59a16fc
juanedcabrera <juanedcabrera@gmail.com>
2023-08-05 14:05:15 -0600

    Updated pages and installed some npm packages

---
+++ src/pages/Jobs.tsx
@@ -0,0 +1,11 @@
+import JobKanbanBoard from '../components/JobKanbanBoard';
+
+function Jobs() {
+  return (
+    <div>
+        <h1>Jobs</h1>
+        <JobKanbanBoard />
+    </div>
+  );
+}
+export default Jobs;
---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,11 @@
+import KanbanBoard from '../components/KanbanBoard';
+
+function Tasks() {
+  return (
+    <div>
+        <h1>Tasks</h1>
+        <KanbanBoard />
+    </div>
+  );
+}
+export default Tasks;
